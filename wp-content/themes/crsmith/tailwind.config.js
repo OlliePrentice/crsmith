4de0031768dd23dde46b3ede127850af46bdb4bb
Ollie Prentice <osprentice@gmail.com>
2021-03-23 10:54:21 +0000

    init

---
+++ wp-content/themes/crsmith/tailwind.config.js
@@ -0,0 +1,59 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  purge: {
+    content: [
+      './*.php',
+      './template-parts/**/*.php',
+      './classes/**/*.php',
+      './inc/**/*.php',
+      './assets/**/*.scss',
+      './assets/**/*.js',
+    ],
+  },
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#003b79',
+        'secondary': '#5caaa8',
+        'tertiary': '#1f2f3e',
+      },
+      fontSize: {
+        'zero': '0',
+        'xxs': '.625rem',
+      },
+      fontFamily: {
+        'display': ['Poppins', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+        'body': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
+      },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ['important'],
+      inset: ['important'],
+      width: ['important'],
+      margin: ['last'],
+      borderColor: ['important'],
+      textColor: ['important']
+    },
+  },
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('important', ({ container }) => {
+        container.walkRules(rule => {
+          rule.selector = `.\\!${rule.selector.slice(1)}`;
+          rule.walkDecls(decl => {
+            decl.important = true;
+          });
+        });
+      });
+    })
+  ],
+};
